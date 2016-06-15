6fd2c3baf8dafe3d0d04d6d77b9f0d09e65c5d6c
arvisura87 <jakab.tamas@me.com>
2016-06-15 11:32:03 +0200

    added spec 1.1

--- selling/selling.py
+++ selling/selling.py
@@ -41,7 +41,7 @@ def start_module():
         elif option == "4":
             update(table, id_)
         elif option == "5":
-            get_lowest_price_item_id(table)
+            ui.print_result(get_lowest_price_item_id(table), "The id of the lowest priced items : ")
         elif option == "6":
             get_items_sold_between(table, month_from, day_from, year_from, month_to, day_to, year_to)
         elif option == "0":
@@ -132,8 +132,7 @@ def get_lowest_price_item_id(table):
         if mini == e:
             final.append(i_d[i])
         i += 1
-    ui.print_result(final, "The id of the lowest priced items : ")
-
+    return final
 
 
 # the question: Which items are sold between two given dates ? (from_date < birth_date < to_date)
